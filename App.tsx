5c0f60fb50043e6457657f2aea31378b469573ca
AhadCove <ahadcove@gmail.com>
2020-09-09 22:19:49 -0400

    Complete

--- App.tsx
+++ App.tsx
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { store, persistor } from './src/redux/store';
+import { PersistGate } from 'redux-persist/lib/integration/react';
+import Home from './src/screens/home/home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <View style={styles.container}>
+          <Home />
+        </View>
+      </PersistGate>
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#20232a',
     alignItems: 'center',
     justifyContent: 'center',
   },
